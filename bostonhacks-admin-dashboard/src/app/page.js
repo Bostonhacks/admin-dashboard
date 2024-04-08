3d652efc48e0cf8a9ff910e5de68a973ed03c5bc
Andrew Liu <andelerliutv@gmail.com>
2024-04-08 17:17:42 -0400

    do :

--- bostonhacks-admin-dashboard/src/app/page.js
+++ bostonhacks-admin-dashboard/src/app/page.js
@@ -1,13 +1,11 @@
-import Authentication from "@/components/LoginPage";
+import LoginPage from "@/components/LoginPage";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div>BostonHacks </div>
-      <Authentication />
-      <Authentication />
-      <Authentication />
-      <Authentication />
+      <LoginPage />
     </main>
   );
 }
+  
--- bostonhacks-admin-dashboard/src/components/LoginPage.jsx
+++ bostonhacks-admin-dashboard/src/components/LoginPage.jsx
@@ -2,6 +2,8 @@ export default function LoginPage () {
     return (
         <div>
             <p>hi how are you</p>
+            <input placeholder="username"></input>
+            <input placeholder="password"></input>
         </div>
     )
 }
