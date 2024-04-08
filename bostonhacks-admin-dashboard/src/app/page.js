5fd3cf56a72f1d1c43982daf8158ab9bbbab826a
michelleelim <michellelim723@gmail.com>
2024-04-07 23:42:58 -0400

    basic login changes

--- bostonhacks-admin-dashboard/src/app/page.js
+++ bostonhacks-admin-dashboard/src/app/page.js
@@ -4,10 +4,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div>BostonHacks </div>
-      <Authentication />
-      <Authentication />
-      <Authentication />
-      <Authentication />
+      <div> <Authentication /></div>
     </main>
   );
 }
--- bostonhacks-admin-dashboard/src/components/LoginPage.jsx
+++ bostonhacks-admin-dashboard/src/components/LoginPage.jsx
@@ -1,7 +1,12 @@
 export default function LoginPage () {
     return (
         <div>
-            <p>hi how are you</p>
+            <center><p>Welcome to BostonHacks!</p></center>
+            <label for="user">Username:</label>
+            <input type="text" id="user" name="user"></input>
+            <label for="pass">Password:</label>
+            <input type="text" id="pass" name="pass"></input><br></br>
+            <center><button class="button"> Login </button></center>      
         </div>
     )
 }
